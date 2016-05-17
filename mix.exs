36a1470f9697659f9c110c3e0c8e371d17957b63
Milan Köpke <mivl@akra.de>
2016-05-18 00:14:57 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mailchimp.Mixfile do
 
   def project do
     [app: :mailchimp,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      description: description,
      package: package,
