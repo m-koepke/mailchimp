259504a412671663cd00e0eb283b9676f1b35855
Milan Köpke <milan.koepke@akra.de>
2018-01-23 21:03:29 +0100

    fix: Parentheses

--- lib/mailchimp.ex
+++ lib/mailchimp.ex
@@ -7,7 +7,7 @@ defmodule Mailchimp do
   def start_link(opts \\ []) do
     shard = get_shard
     apiroot = "https://#{shard}.api.mailchimp.com/3.0/"
-    config = %{apiroot: apiroot, apikey: Application.get_env :mailchimp, :apikey}
+    config = %{apiroot: apiroot, apikey: Application.get_env(:mailchimp, :apikey)}
     GenServer.start_link(Mailchimp, config, name: :mailchimp)
   end
 
@@ -80,14 +80,14 @@ defmodule Mailchimp do
   end
 
   def get_shard do
-    parts = Application.get_env :mailchimp, :apikey
+    parts = Application.get_env(:mailchimp, :apikey)
     |> String.split(~r{-})
 
     case length(parts) do
       2 ->
         List.last parts
       _ ->
-        Logger.error "This doesn't look like an API Key: #{Application.get_env :mailchimp, :apikey}"
+        Logger.error "This doesn't look like an API Key: #{Application.get_env(:mailchimp, :apikey)}"
         Logger.info "The API Key should have both a key and a server name, separated by a dash, like this: abcdefg8abcdefg6abcdefg4-us1"
         {:error}
     end
